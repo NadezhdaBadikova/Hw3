998d37cc846555a8e6ac50e38178e78b8e25d6cc
Nadezhda <nb-coduz@mail.ru>
2022-12-05 22:18:19 +0300

    zadacha1

---
+++ 1.py
@@ -0,0 +1,17 @@
+# 1'. Задайте список из нескольких чисел. Напишите программу, которая найдёт 
+# сумму элементов списка, стоящих на нечётной позиции.
+# *Пример:*
+# - [2, 3, 5, 9, 3] -> на нечётных позициях элементы 3 и 9, ответ: 12
+
+
+import os
+os.system('cls||clear')
+
+from random import randint
+list = [randint(1, 10) for i in range(5)]
+print (list)
+sum = 0
+for i in range(len(list)):
+    if i % 2 != 0:
+        sum += list[i]
+print(f"Сумма равна: {sum}")
